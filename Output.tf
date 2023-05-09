449d8114bfd1bb0d0a48eed0a0ac556e378cef39
Patrick Affadu <kaffadu@icloud.com>
2023-05-09 18:42:50 +0100

    Variables and Outputs Added

---
+++ Output.tf
@@ -0,0 +1,43 @@
+output "vpc_id" {
+    value = aws_vpc.tenacity-vpc.id
+}
+
+output "vpc_cidr_block" {
+    value = aws_vpc.tenacity-vpc.cidr_block
+}
+
+output "aws_Prod-pub-sub1_cidr" {
+    value = aws_subnet.Prod-pub-sub1.cidr_block
+}
+
+output "aws_Prod-pub-sub2_cidr" {
+    value = aws_subnet.Prod-pub-sub2.cidr_block
+}
+
+output "aws_Prod-priv-sub1_cidr" {
+    value = aws_subnet.Prod-priv-sub1.cidr_block
+}
+
+output "aws_Prod-priv-sub2_cidr" {
+    value = aws_subnet.Prod-priv-sub2.cidr_block
+}
+
+output "aws_Prod-pub_route_table" {
+    value = aws_route_table.Prod-pub-route-table.id
+}
+
+output "aws_Prod-priv_route_table" {
+    value = aws_route_table.Prod-priv-route-table.id
+}
+
+output "aws_internet_gateway" {
+    value = aws_internet_gateway.Prod-igw.id
+}
+
+output "aws_eip" {
+    value = aws_eip.eip1.id
+}
+
+output "aws_Nat_gateway" {
+    value = aws_nat_gateway.Prod-Nat-gateway.id
+}
---
+++ main_Var.tf
@@ -0,0 +1,141 @@
+# Tenacity IT Group VPC
+
+resource "aws_vpc" "tenacity-vpc" {
+  cidr_block = var.cidr_block
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "${var.environment}-vpc"
+  }
+}
+
+# Tenacity Subnets
+
+resource "aws_subnet" "Prod-pub-sub1" {
+  vpc_id     = "${aws_vpc.tenacity-vpc.id}"
+  cidr_block = var.Prod-pub-sub1_cidr
+
+  tags = {
+    Name = "${var.environment}-Prod-pub-sub1"
+  }
+}
+
+resource "aws_subnet" "Prod-pub-sub2" {
+  vpc_id     = "${aws_vpc.tenacity-vpc.id}"
+  cidr_block = var.Prod-pub-sub2_cidr
+
+  tags = {
+    Name = "${var.environment}-Prod-pub-sub2"
+  }
+}
+
+resource "aws_subnet" "Prod-priv-sub1" {
+  vpc_id     = "${aws_vpc.tenacity-vpc.id}"
+  cidr_block = var.Prod-priv-sub1_cidr
+
+  tags = {
+    Name = "${var.environment}-Prod-priv-sub1"
+  }
+}
+
+resource "aws_subnet" "Prod-priv-sub2" {
+  vpc_id     = "${aws_vpc.tenacity-vpc.id}"
+  cidr_block = var.Prod-priv-sub2_cidr
+
+  tags = {
+    Name = "${var.environment}-Prod-priv-sub2"
+  }
+}
+
+# Tenacity Route Tables & Tenacity Internet Gateway Route Association
+
+resource "aws_route_table" "Prod-pub-route-table" {
+  vpc_id = "${aws_vpc.tenacity-vpc.id}"
+  /*route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.Prod-igw.id
+  }*/
+  tags = {
+    Name = "${var.environment}-Prod-pub-route-table"
+  }
+}
+
+resource "aws_route_table" "Prod-priv-route-table" {
+  vpc_id = "${aws_vpc.tenacity-vpc.id}"
+  /*route {
+      cidr_block = "0.0.0.0/0"
+      nat_gateway_id = aws_nat_gateway.Prod-Nat-gateway.id
+  }*/ 
+  tags = {
+    Name = "${var.environment}-Prod-priv-route-table"
+  }
+}
+
+# Tenacity Subnets Associations
+
+resource "aws_route_table_association" "pub-Sub1-Association" {
+  subnet_id      = aws_subnet.Prod-pub-sub1.id
+  route_table_id = aws_route_table.Prod-pub-route-table.id
+}
+
+resource "aws_route_table_association" "pub-Sub2-Association" {
+  subnet_id      = aws_subnet.Prod-pub-sub2.id
+  route_table_id = aws_route_table.Prod-pub-route-table.id
+}
+
+resource "aws_route_table_association" "priv-Sub1-Association" {
+  subnet_id      = aws_subnet.Prod-priv-sub1.id
+  route_table_id = aws_route_table.Prod-priv-route-table.id
+}
+
+resource "aws_route_table_association" "priv-Sub2-Association" {
+  subnet_id      = aws_subnet.Prod-priv-sub2.id
+  route_table_id = aws_route_table.Prod-priv-route-table.id
+}
+
+# Tenacity Internet Gateway
+
+resource "aws_internet_gateway" "Prod-igw" {
+  vpc_id = "${aws_vpc.tenacity-vpc.id}"
+
+  tags = {
+    Name = "${var.environment}-Prod-igw"
+  }
+}
+ 
+ # Associating Internet Gateway to Public Route Table
+
+resource "aws_route" "Prod-igw-association" {
+  route_table_id = aws_route_table.Prod-pub-route-table.id
+  gateway_id = aws_internet_gateway.Prod-igw.id
+  destination_cidr_block = "0.0.0.0/0" 
+}
+
+
+# Elastic ip
+
+resource "aws_eip" "eip1" {
+  vpc = var.aws_eip
+tags = {
+    Name = "${var.environment}-Elastic IP"
+  }
+}
+
+# Tenacity NAT Gateway
+
+resource "aws_nat_gateway" "Prod-Nat-gateway" {
+  allocation_id = aws_eip.eip1.id
+  subnet_id     = aws_subnet.Prod-pub-sub1.id
+
+  tags = {
+    Name = "${var.environment}-Prod-Nat-gateway"
+  }
+}
+
+ # Associating NAT Gateway to Private Route Table
+
+resource "aws_route" "Prod-Nat-association" {
+  route_table_id = aws_route_table.Prod-priv-route-table.id
+  gateway_id = aws_nat_gateway.Prod-Nat-gateway.id
+  destination_cidr_block = "0.0.0.0/0"
+}
